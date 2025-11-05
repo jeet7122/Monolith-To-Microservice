26cc0ac3f4315d08af04f3797abb51fea06e0fc5
jeet7122 <jeetthakkar2612@gmail.com>
2025-11-05 13:46:46 -0500

    Initial Commit

---
+++ src/main/java/com/jobsearch/job_app/JobAppApplication.java
@@ -0,0 +1,13 @@
+package com.jobsearch.job_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobAppApplication.class, args);
+	}
+
+}
